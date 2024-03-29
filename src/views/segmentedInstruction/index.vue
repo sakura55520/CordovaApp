b0a7da0877f712ae8c1f0793ccfb07f86fc04394
longzeqiu <1093632064@qq.com>
2024-03-29 16:39:15 +0800

    [开发]优化晶锭检测

--- src/views/segmentedInstruction/index.vue
+++ src/views/segmentedInstruction/index.vue
@@ -334,40 +334,47 @@
                   }%`,
                 }"
               >
-                <div class="number">
-                  <div v-if="index === 0" class="headPosition">
-                    {{ item.headPosition }}
+                <div
+                  v-if="
+                    (item.headPosition || item.headPosition === 0) &&
+                    (item.tailPosition || item.tailPosition === 0)
+                  "
+                >
+                  <div class="number">
+                    <div v-if="index === 0" class="headPosition">
+                      {{ item.headPosition }}
+                    </div>
+                    <div class="tailPosition">{{ item.tailPosition }}</div>
                   </div>
-                  <div class="tailPosition">{{ item.tailPosition }}</div>
+                  <div class="line"></div>
+                  <el-tooltip placement="bottom" effect="light">
+                    <template #content>
+                      <div>直径：{{ item.diameter }}</div>
+                      <div>段位：{{ item.segmentNum }}</div>
+                      <div>晶锭长度：{{ item.length }}</div>
+                      <div>计划重量：{{ item.planWeight }}</div>
+                      <div>头部电阻率：{{ item.headResistance }}</div>
+                      <div>尾部电阻率：{{ item.tailResistance }}</div>
+                      <div>头部电阻率实测：{{ item.headResistanceActual }}</div>
+                      <div>尾部电阻率实测：{{ item.tailResistanceActual }}</div>
+                      <div>79oi头：{{ item.head79oi }}</div>
+                      <div>79oi尾：{{ item.tail79oi }}</div>
+                      <div>
+                        合格状态：{{ item.qualified ? "合格" : "不合格" }}
+                      </div>
+                      <div>合格长度：{{ item.qualifiedLength }}</div>
+                      <div>合格重量：{{ item.qualifiedWeight }}</div>
+                    </template>
+                    <div
+                      :class="selectedIndex === index ? 'bar-selected' : 'bar'"
+                      @click="handleSegmentedBarClick(index)"
+                    >
+                      <div class="center">
+                        {{ item.segmentNo }}
+                      </div>
+                    </div>
+                  </el-tooltip>
                 </div>
-                <div class="line"></div>
-                <el-tooltip placement="bottom" effect="light">
-                  <template #content>
-                    <div>直径：{{ item.diameter }}</div>
-                    <div>段位：{{ item.segmentNum }}</div>
-                    <div>晶锭长度：{{ item.length }}</div>
-                    <div>计划重量：{{ item.planWeight }}</div>
-                    <div>头部电阻率：{{ item.headResistance }}</div>
-                    <div>尾部电阻率：{{ item.tailResistance }}</div>
-                    <div>头部电阻率实测：{{ item.headResistanceActual }}</div>
-                    <div>尾部电阻率实测：{{ item.tailResistanceActual }}</div>
-                    <div>79oi头：{{ item.head79oi }}</div>
-                    <div>79oi尾：{{ item.tail79oi }}</div>
-                    <div>
-                      合格状态：{{ item.qualified ? "合格" : "不合格" }}
-                    </div>
-                    <div>合格长度：{{ item.qualifiedLength }}</div>
-                    <div>合格重量：{{ item.qualifiedWeight }}</div>
-                  </template>
-                  <div
-                    :class="selectedIndex === index ? 'bar-selected' : 'bar'"
-                    @click="handleSegmentedBarClick(index)"
-                  >
-                    <div class="center">
-                      {{ item.segmentNo }}
-                    </div>
-                  </div>
-                </el-tooltip>
               </div>
             </div>
           </div>
