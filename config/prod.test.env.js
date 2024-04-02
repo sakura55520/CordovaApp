85ccca19a7d975b1e42fc1ed8b842c72d439b0da
renjihua <151398296180oO>
2024-04-02 14:57:31 +0800

    build: 测试环境ip

--- config/prod.test.env.js
+++ config/prod.test.env.js
@@ -1,9 +1,9 @@
-// 现场测试环境 暂时用不到
+// 现场测试环境
 module.exports = {
   NODE_ENV: '"production_test"',
   // 生产测试
-  BASE_API: '"http://10.60.120.56:90/MES_SERVER"',
-  BASE_API_FILEHOST: '"http://10.60.120.56:90/MES_SERVER"',
-  BASE_API_FILEPREVIEW:'"http://47.99.105.168:8888/"',
+  BASE_API: '"http://172.19.100.50:90/MES_SERVER"',
+  BASE_API_FILEHOST: '"http://172.19.100.50:90/MES_SERVER"',
+  BASE_API_FILEPREVIEW:'"http://172.19.100.50:8888/"',
   MQ_PASS: '"admin"'
 }
