efd6e3f42ee20f33cc1de5b24236df7a32f5ea31
qiukeren <qiuker521@163.com>
2024-05-28 18:11:04 +0800

    增加 钟罩类型扫码功能

---
+++ src/components/SelectBellCoverType.vue
@@ -0,0 +1,145 @@
+<!--辅料寿命-->
+<template>
+  <el-row :gutter="8">
+    <el-col v-if="!disabled" :span="9">
+      <el-form-item :label="calcScanLabel" class="pre-label">
+        <CodeScanner v-model="valueScan" @has-done="findInventoryByCode"/>
+      </el-form-item>
+    </el-col>
+    <el-col :span="6">
+      <el-form-item :label="`钟罩类型`" class="pre-label">
+        <el-input :value="coverType" disabled/>
+      </el-form-item>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import {findInventoryByCode} from "@/api/inStation";
+
+export default {
+  name: "SelectBellCoverType",
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: Object,
+      default() {
+        return {
+          // 编号
+          objCode: {
+            extKey: this.label,
+            extValue: ''
+          },
+          // 编号(扫码)
+          objScan: {
+            extKey: this.fieldScan,
+            extValue: ''
+          },
+          // 已使用寿命/额定寿命
+          objLife: {
+            extKey: this.fieldLife,
+            extValue: ''
+          },
+        }
+      }
+    },
+    fieldScan: {
+      type: String,
+      default: ''
+    },
+    fieldLife: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      warnStatus: 0, // 0: 未预警, 1: 已预警
+    }
+  },
+  computed: {
+    // 编号
+    valueCode() {
+      const { objCode } = this.value || {}
+      return objCode ? objCode.extValue : ''
+    },
+    // 扫码value
+    valueScan: {
+      get() {
+        const { objScan } = this.value || {}
+        return objScan ? objScan.extValue : ''
+      },
+      set(extValue) {
+        this.$emit('input', {
+          ...this.value,
+          objScan: {
+            ...this.value.objScan,
+            extKey: this.fieldScan,
+            extValue
+          }
+        })
+      }
+    },
+    coverType: {
+      get() {
+        const { objLife } = this.value || {}
+        return objLife ? objLife.extValue : ''
+      },
+      set(extValue) {
+        this.$emit('input', {
+          ...this.value,
+          objLife: {
+            ...this.value.objLife,
+            extKey: this.fieldLife,
+            extValue
+          }
+        })
+      }
+    },
+    calcScanLabel() {
+      return this.fieldScan.replace(/[（(]/, match => '\n' + match)
+    },
+    calcLifeLabel() {
+      return this.fieldLife.replace(/\//, match => '\n' + match)
+    }
+  },
+  methods: {
+    findInventoryByCode(code) {
+      if (!code) return
+      if (this.valueCode && code !== this.valueCode) return this.$message.warning(`扫描编号必须为${this.valueCode}`)
+      findInventoryByCode({
+        search_EQ_uniqueCode: code
+      }).then(res => {
+        let coverType
+        const { data } = res
+        if (data && data[0]) {
+          coverType = data[0].materialTypeName
+          this.warnStatus = data[0].warnStatus
+          if (this.warnStatus) {
+            this.$message.warning(`【${code}】寿命达到预警值!`)
+          } else {
+            this.$message.success(`【${code}】寿命查询成功!`)
+          }
+        } else {
+          this.$message.warning(`【${code}】未查询到寿命信息!`)
+        }
+        this.coverType = (coverType || '')
+      })
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-form-item {
+  width: 100% !important;
+}
+</style>
+
