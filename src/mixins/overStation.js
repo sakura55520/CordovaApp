5c41f04090a02df51e7af587d91dad83655f62db
龙泽球 <longzeqiu@cczn.cc>
2024-06-27 13:50:19 +0800

    [开发]历史过站详情增加执行设备

--- src/mixins/overStation.js
+++ src/mixins/overStation.js
@@ -1,11 +1,11 @@
-import {postMessage} from "@/utils/overStation";
-import {getSeleteData} from "@/utils/select";
+import { postMessage } from "@/utils/overStation";
+import { getSeleteData } from "@/utils/select";
 
 export default {
   data() {
     return {
-      enableMap: {} // 字段是否允许编辑
-    }
+      enableMap: {}, // 字段是否允许编辑
+    };
   },
   computed: {
     isEnd() {
@@ -16,44 +16,45 @@ export default {
         .productionEquipmentCode;
     },
     executionEquipment() {
-      return this.$route.query.deviceCode;
+      return this.$route.query.deviceCode || this.$route.query.equipmentCode;
     },
   },
   methods: {
     async back(msg, operator) {
-      if (operator === 'confirm') {
-        await this.$confirm('系统不会保存您填写的内容, 确认取消吗？', '提示', {
-          type: 'warning'
-        })
+      if (operator === "confirm") {
+        await this.$confirm("系统不会保存您填写的内容, 确认取消吗？", "提示", {
+          type: "warning",
+        });
       }
       if (window._pc_window) {
-        postMessage(msg)
-      }
-      else {
-        msg && this.$message.success(msg)
-        this.$router.back()
+        postMessage(msg);
+      } else {
+        msg && this.$message.success(msg);
+        this.$router.back();
       }
     },
     fetchSwitchDict() {
-      const nameSpace = this.$route.query.wipStorageCode
-      const list = []
-      this.enableMap = {}
+      const nameSpace = this.$route.query.wipStorageCode;
+      const list = [];
+      this.enableMap = {};
       // 系统全局开关 字典
-      getSeleteData('global_switch', list).then(() => {
+      getSeleteData("global_switch", list).then(() => {
         list.forEach(({ name, value, extendValue, extendValue1 }) => {
-          if (name !== nameSpace || !value) return
+          if (name !== nameSpace || !value) return;
 
-          const enable = extendValue === '1'
-          this.$set(this.enableMap, value, enable)
+          const enable = extendValue === "1";
+          this.$set(this.enableMap, value, enable);
 
-          const required = extendValue1 === '1'
+          const required = extendValue1 === "1";
           if (this.formRules[value] && this.formRules[value][0]) {
-            this.formRules[value][0].required = required
+            this.formRules[value][0].required = required;
           } else if (required) {
-            this.$set(this.formRules, value, [{ required, message: "必填项", trigger: "change" }])
+            this.$set(this.formRules, value, [
+              { required, message: "必填项", trigger: "change" },
+            ]);
           }
-        })
-      })
-    }
-  }
-}
+        });
+      });
+    },
+  },
+};
