03a55270382b750da10429e00ebda80d4ef8f9db
龙泽球 <longzeqiu@cczn.cc>
2024-07-02 17:23:44 +0800

    [开发]格式化

--- src/views/accessory/index.vue
+++ src/views/accessory/index.vue
@@ -8,11 +8,7 @@
 -->
 <template>
   <div class="from">
-    <el-form
-      :model="searchList"
-      class="acc-form"
-      @submit.native.prevent
-    >
+    <el-form :model="searchList" class="acc-form" @submit.native.prevent>
       <el-form-item>
         <el-radio-group v-model="listType" @change="searchRows">
           <el-radio-button label="accessory">辅材明细</el-radio-button>
@@ -33,11 +29,7 @@
             class="el-input__icon el-icon-circle-close"
             @click="clearData"
           />
-          <el-button
-            slot="append"
-            icon="el-icon-search"
-            @click="searchRows"
-          />
+          <el-button slot="append" icon="el-icon-search" @click="searchRows" />
         </el-input>
       </el-form-item>
     </el-form>
@@ -46,20 +38,35 @@
       <div class="menu">
         <el-form :model="dialogForm">
           <el-form-item label="预警状态" v-if="listType === 'accessory'">
-            <el-checkbox-group v-model="dialogForm.warnStatus" size="" >
-              <el-checkbox-button v-for="(item,index) in warnStatusMap" :label="item.name" :key="index">{{item.name}}</el-checkbox-button>
+            <el-checkbox-group v-model="dialogForm.warnStatus" size="">
+              <el-checkbox-button
+                v-for="(item, index) in warnStatusMap"
+                :label="item.name"
+                :key="index"
+                >{{ item.name }}</el-checkbox-button
+              >
             </el-checkbox-group>
           </el-form-item>
 
           <el-form-item label="使用状态" v-if="listType === 'accessory'">
-            <el-checkbox-group v-model="dialogForm.status" size="" >
-              <el-checkbox-button v-for="(item,index) in useStatusMap" :label="item.name" :key="index">{{item.name}}</el-checkbox-button>
+            <el-checkbox-group v-model="dialogForm.status" size="">
+              <el-checkbox-button
+                v-for="(item, index) in useStatusMap"
+                :label="item.name"
+                :key="index"
+                >{{ item.name }}</el-checkbox-button
+              >
               <!-- <el-checkbox-button v-if="listType === 'apply'" style="margin-left: 1px;" label="待出库"></el-checkbox-button> -->
             </el-checkbox-group>
           </el-form-item>
-           <el-form-item label="使用状态" v-if="listType === 'apply'">
-            <el-checkbox-group v-model="dialogForm.status" size="" >
-              <el-checkbox-button v-for="(item,index) in usewordStatusMap" :label="item.name" :key="index">{{item.name}}</el-checkbox-button>
+          <el-form-item label="使用状态" v-if="listType === 'apply'">
+            <el-checkbox-group v-model="dialogForm.status" size="">
+              <el-checkbox-button
+                v-for="(item, index) in usewordStatusMap"
+                :label="item.name"
+                :key="index"
+                >{{ item.name }}</el-checkbox-button
+              >
               <!-- <el-checkbox-button v-if="listType === 'apply'" style="margin-left: 1px;" label="待出库"></el-checkbox-button> -->
             </el-checkbox-group>
           </el-form-item>
@@ -73,28 +80,83 @@
         highlight-current-row
         class="admin_table"
       >
-        <el-table-column prop="materialCode" key="materialCode" label="料号" width="120" fixed="left"/>
-        <el-table-column prop="materialName" key="materialName" label="物料名称" min-width="150"/>
-        <el-table-column prop="equipmentCode" key="equipmentCode" label="设备" min-width="150"/>
+        <el-table-column
+          prop="materialCode"
+          key="materialCode"
+          label="料号"
+          width="140"
+          fixed="left"
+        />
+        <el-table-column
+          prop="materialName"
+          key="materialName"
+          label="物料名称"
+          min-width="150"
+        />
+        <el-table-column
+          prop="equipmentCode"
+          key="equipmentCode"
+          label="设备"
+          min-width="150"
+        />
 
         <template v-if="listType === 'accessory'">
-          <el-table-column prop="uniqueCode" key="uniqueCode" label="唯一码" width="150"/>
-          <el-table-column prop="ratedLife" key="ratedLife" label="额定寿命" width="100"/>
-          <el-table-column prop="usefulLife" key="usefulLife" label="已使用寿命" width="100"/>
-          <el-table-column prop="remainingLife" key="remainingLife" label="剩余寿命" width="100"/>
-          <el-table-column prop="warnLife" label="预警寿命"  width="100">
-           <template slot-scope="scope"  >
-              <div  >{{scope.row.warnLife}}<img v-show="scope.row.warnStatus == 1" src="@/assets/gif/warning.gif"></div>
+          <el-table-column
+            prop="uniqueCode"
+            key="uniqueCode"
+            label="唯一码"
+            width="150"
+          />
+          <el-table-column
+            prop="ratedLife"
+            key="ratedLife"
+            label="额定寿命"
+            width="100"
+          />
+          <el-table-column
+            prop="usefulLife"
+            key="usefulLife"
+            label="已使用寿命"
+            width="100"
+          />
+          <el-table-column
+            prop="remainingLife"
+            key="remainingLife"
+            label="剩余寿命"
+            width="100"
+          />
+          <el-table-column prop="warnLife" label="预警寿命" width="100">
+            <template slot-scope="scope">
+              <div>
+                {{ scope.row.warnLife
+                }}<img
+                  v-show="scope.row.warnStatus == 1"
+                  src="@/assets/gif/warning.gif"
+                />
+              </div>
             </template>
           </el-table-column>
-          <el-table-column prop="accessoryStatus" key="accessoryStatus" label="物料状态" width="100">
+          <el-table-column
+            prop="accessoryStatus"
+            key="accessoryStatus"
+            label="物料状态"
+            width="100"
+          >
             <template slot-scope="scope">
-              <el-tag v-if="scope.row.transMap" class="tag">{{ scope.row.transMap.accessoryStatusName }}</el-tag>
+              <el-tag v-if="scope.row.transMap" class="tag">{{
+                scope.row.transMap.accessoryStatusName
+              }}</el-tag>
             </template>
           </el-table-column>
         </template>
 
-        <el-table-column prop="status" key="status" label="使用状态" width="110" fixed="right">
+        <el-table-column
+          prop="status"
+          key="status"
+          label="使用状态"
+          width="110"
+          fixed="right"
+        >
           <template slot-scope="scope">
             <el-tag class="tag">{{ transformStatus(scope.row.status) }}</el-tag>
           </template>
@@ -109,15 +171,17 @@
                 type="danger"
                 plain
                 @click="handleReturn(scope.row)"
-              >退库</el-button>
+                >退库</el-button
+              >
 
               <el-button
-               v-if="scope.row.status === 0"
+                v-if="scope.row.status === 0"
                 icon="el-icon-document-remove"
                 type="danger"
                 plain
                 @click="upComputer(scope.row)"
-              >上机</el-button>
+                >上机</el-button
+              >
 
               <el-button
                 v-if="scope.row.status === 2"
@@ -125,7 +189,8 @@
                 type="info"
                 plain
                 @click="handleCancelReturn(scope.row)"
-              >取消退库</el-button>
+                >取消退库</el-button
+              >
 
               <el-button
                 v-if="scope.row.status === 2"
@@ -133,7 +198,8 @@
                 type="primary"
                 plain
                 @click="handleApply(scope.row)"
-              >申请</el-button>
+                >申请</el-button
+              >
             </template>
 
             <template v-else>
@@ -143,9 +209,9 @@
                 type="info"
                 plain
                 @click="handleCancelApply(scope.row)"
-              >取消申请</el-button>
+                >取消申请</el-button
+              >
             </template>
-
           </div>
         </el-table-column>
       </el-table>
@@ -156,7 +222,8 @@
         layout="total, prev, pager, next"
         class="admin_pagination"
         @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"/>
+        @current-change="handleCurrentChange"
+      />
     </div>
 
     <!--退库弹窗-->
@@ -173,22 +240,22 @@
 </template>
 
 <script>
-import * as Api from '@/api/accessory'
-import { getSeleteData, getEnumDatas } from '@/utils/select'
-import ReturnDialog from './ReturnDialog'
-import ApplyDialog from './ApplyDialog'
-import DownDialog from './DownDialog'
+import * as Api from "@/api/accessory";
+import { getSeleteData, getEnumDatas } from "@/utils/select";
+import ReturnDialog from "./ReturnDialog";
+import ApplyDialog from "./ApplyDialog";
+import DownDialog from "./DownDialog";
 
 const defaultSearchList = {
   search_EQ_equipmentCode: null,
-}
+};
 // 根据data返回的每一行的数据判断,再修改这一行的样式
 
 export default {
-  name: 'Accessory',
+  name: "Accessory",
   data() {
     return {
-      listType: 'accessory',
+      listType: "accessory",
       searchList: Object.assign({}, defaultSearchList),
       tableList: [],
       currentPage: 1,
@@ -201,164 +268,191 @@ export default {
       accessoriesStatusMap: [], // 物料状态map
       useStatusMap: [], // 使用状态map
       currentComponent: null,
-      warnStatusMap:[],
-      usewordStatusMap:[],
-      warnStatusChange:false,
+      warnStatusMap: [],
+      usewordStatusMap: [],
+      warnStatusChange: false,
       dialogForm: {
-        warnStatus:[''],
-        status:['']
-    }
-    }
-  },
-   watch: {
-    dialogForm:{
-      handler(val){
-        this.currentPage = 1
-        this.fetchData()
-
+        warnStatus: [""],
+        status: [""],
       },
-      deep:true
-    }
+    };
+  },
+  watch: {
+    dialogForm: {
+      handler(val) {
+        this.currentPage = 1;
+        this.fetchData();
+      },
+      deep: true,
+    },
   },
   created() {
-    getEnumDatas('辅材物料状态', this.accessoriesStatusMap) // 物料状态
-    getSeleteData('useStatus', this.useStatusMap) // 使用状态map
-    getSeleteData('warnStatus',this.warnStatusMap)
-    getSeleteData('wordUseStatus',this.usewordStatusMap)
-    this.fetchData()
+    getEnumDatas("辅材物料状态", this.accessoriesStatusMap); // 物料状态
+    getSeleteData("useStatus", this.useStatusMap); // 使用状态map
+    getSeleteData("warnStatus", this.warnStatusMap);
+    getSeleteData("wordUseStatus", this.usewordStatusMap);
+    this.fetchData();
   },
   methods: {
-    cellStyle (data){
-    if (data.row.warnStatus == 1) {
-      return {
-        background: "#FEF3BD",
-      };
-    }
-  },
+    cellStyle(data) {
+      if (data.row.warnStatus == 1) {
+        return {
+          background: "#FEF3BD",
+        };
+      }
+    },
     fetchData() {
-      const fetch = this.listType === 'accessory' ? Api.fetchData : Api.fetchApply
-      let statussList = []
-    if(this.listType === 'accessory'){
+      const fetch =
+        this.listType === "accessory" ? Api.fetchData : Api.fetchApply;
+      let statussList = [];
+      if (this.listType === "accessory") {
         statussList = this.dialogForm.status
-      .filter(item => this.useStatusMap.some(val => item === val.name))
-      .map(item => this.useStatusMap.find(val => item === val.name).value);
-    }else{
+          .filter((item) => this.useStatusMap.some((val) => item === val.name))
+          .map(
+            (item) => this.useStatusMap.find((val) => item === val.name).value
+          );
+      } else {
         statussList = this.dialogForm.status
-      .filter(item => this.usewordStatusMap.some(val => item === val.name))
-      .map(item => this.usewordStatusMap.find(val => item === val.name).value);
-    }
+          .filter((item) =>
+            this.usewordStatusMap.some((val) => item === val.name)
+          )
+          .map(
+            (item) =>
+              this.usewordStatusMap.find((val) => item === val.name).value
+          );
+      }
 
       const warnStatusList = this.dialogForm.warnStatus
-      .filter(item => this.warnStatusMap.some(val => item === val.name))
-      .map(item => this.warnStatusMap.find(val => item === val.name).value);
+        .filter((item) => this.warnStatusMap.some((val) => item === val.name))
+        .map(
+          (item) => this.warnStatusMap.find((val) => item === val.name).value
+        );
 
-      console.log(statussList.join(','),"statussList"),
-      fetch({
-        search_IN_warnStatus: warnStatusList.join(','),
-        search_IN_status: statussList.join(','),
-        ...this.searchList,
-        rows: this.pageSize,
-        page: this.currentPage
-      }).then(res => {
-        this.tableList = res.data.rows
-        this.total = parseInt(res.data.total)
-        this.$nextTick(() => {
-          this.$refs['table'].doLayout()
-        })
-      })
+      console.log(statussList.join(","), "statussList"),
+        fetch({
+          search_IN_warnStatus: warnStatusList.join(","),
+          search_IN_status: statussList.join(","),
+          ...this.searchList,
+          rows: this.pageSize,
+          page: this.currentPage,
+        }).then((res) => {
+          this.tableList = res.data.rows;
+          this.total = parseInt(res.data.total);
+          this.$nextTick(() => {
+            this.$refs["table"].doLayout();
+          });
+        });
     },
     // 退库
     handleReturn(row) {
-      this.dialogVisible = true
-      this.currentComponent = ReturnDialog
-      this.row = row
+      this.dialogVisible = true;
+      this.currentComponent = ReturnDialog;
+      this.row = row;
     },
     //下机
-    downComputer(row){
-      this.currentComponent = DownDialog
-      this.dialogVisible = true
-      this.row = row
+    downComputer(row) {
+      this.currentComponent = DownDialog;
+      this.dialogVisible = true;
+      this.row = row;
     },
     //上机
-    upComputer({ id, materialCode }){
-       this.$confirm(`${materialCode} 即将进行【上机】操作, 请确定`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        Api.upComputer(id).then(res => {
-          this.$message({ type: 'success', message: '已上机!' })
-          this.fetchData()
+    upComputer({ id, materialCode }) {
+      this.$confirm(`${materialCode} 即将进行【上机】操作, 请确定`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          Api.upComputer(id).then((res) => {
+            this.$message({ type: "success", message: "已上机!" });
+            this.fetchData();
+          });
         })
-      }).catch(() => {})
+        .catch(() => {});
     },
     // 取消退库
     handleCancelReturn({ id, materialCode }) {
-      this.$confirm(`${materialCode} 即将进行【取消退库】操作, 请确定`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        Api.cancelStockReturn(id).then(res => {
-          this.$message({ type: 'success', message: '已取消退库!' })
-          this.fetchData()
+      this.$confirm(
+        `${materialCode} 即将进行【取消退库】操作, 请确定`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          Api.cancelStockReturn(id).then((res) => {
+            this.$message({ type: "success", message: "已取消退库!" });
+            this.fetchData();
+          });
         })
-      }).catch(() => {})
+        .catch(() => {});
     },
     // 申请
     handleApply(row) {
-      this.currentComponent = ApplyDialog
-      this.dialogVisible = true
-      this.row = row
+      this.currentComponent = ApplyDialog;
+      this.dialogVisible = true;
+      this.row = row;
     },
     // 取消申请
     handleCancelApply({ id, materialCode }) {
-      this.$confirm(`${materialCode} 即将进行【取消申请】操作, 请确定`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        Api.cancelApplyAccessory(id).then(res => {
-          this.$message({ type: 'success', message: '已取消申请!' })
-          this.fetchData()
+      this.$confirm(
+        `${materialCode} 即将进行【取消申请】操作, 请确定`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          Api.cancelApplyAccessory(id).then((res) => {
+            this.$message({ type: "success", message: "已取消申请!" });
+            this.fetchData();
+          });
         })
-      }).catch(() => {})
+        .catch(() => {});
     },
     transformStatus(status) {
-      let matched=''
-      if(this.listType === 'accessory'){
-         matched = this.useStatusMap.find(item => item.value === String(status))
-      }else{
-          matched = this.usewordStatusMap.find(item => item.value === String(status))
+      let matched = "";
+      if (this.listType === "accessory") {
+        matched = this.useStatusMap.find(
+          (item) => item.value === String(status)
+        );
+      } else {
+        matched = this.usewordStatusMap.find(
+          (item) => item.value === String(status)
+        );
       }
-      return matched ? matched.name : status
+      return matched ? matched.name : status;
     },
     searchRows() {
-      this.currentPage = 1
-      this.fetchData()
+      this.currentPage = 1;
+      this.fetchData();
     },
     clearData() {
-      this.searchList = Object.assign({}, defaultSearchList)
-      this.searchRows()
+      this.searchList = Object.assign({}, defaultSearchList);
+      this.searchRows();
     },
     handleSizeChange(val) {
-      this.pageSize = val
-      this.fetchData()
+      this.pageSize = val;
+      this.fetchData();
     },
     handleCurrentChange(val) {
-      this.currentPage = val
-      this.fetchData()
-    }
-  }
-}
+      this.currentPage = val;
+      this.fetchData();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .menu{
-    padding: 5px;
-    width: 100%;
-    border: none;
-  }
+.menu {
+  padding: 5px;
+  width: 100%;
+  border: none;
+}
 .acc-form {
   margin-top: 16px;
   display: flex;
